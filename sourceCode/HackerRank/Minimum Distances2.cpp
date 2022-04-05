7fcd60bc4869a39149aa37569e1d58854db5e823
inchency <donghon92@gmail.com>
2022-04-05 21:44:43 +0900

    Minimum Distances (easy) 시간복잡도 O(N) 풀이(N = max(a))

---
+++ sourceCode/HackerRank/Minimum Distances2.cpp
@@ -0,0 +1,105 @@
+// https://www.hackerrank.com/challenges/minimum-distances/problem?isFullScreen=true
+// O(max (a[i])) 풀이
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'minimumDistances' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+int minimumDistances(vector<int> a) {
+    int max_num = *max_element(a.begin(), a.end());
+    vector<int>v(max_num+1, -1);
+    int answer = INT_MAX;
+    for(int i = 0; i<a.size(); i++){
+        int i_num = a[i];
+        if(v[i_num] == -1)
+            v[i_num] = i;
+        else{
+            int diff = i - v[i_num];
+            answer = min(answer, diff);
+            v[i_num] = i;
+        }
+    }
+    if(answer == INT_MAX) return -1;
+    return answer;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split(rtrim(a_temp_temp));
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    int result = minimumDistances(a);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
